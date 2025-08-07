3cd26c583b47b7e8b51eef5f923d74a2c5fb7fa0
AnjaliPhukan <zara.phukan@gmail.com>
2025-08-07 15:23:34 -0300

    added a help page with the --help optional argument. Added better error handling for command arguments and file reading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::io::ErrorKind;
 
 fn main() {
     let mut args_iter = env::args().collect::<Vec<String>>().into_iter();
@@ -7,21 +8,46 @@ fn main() {
 
     let mut essential_args = Vec::<String>::with_capacity(2);
     let mut title: Option<String> = None;
-
+    // loops over command line arguments to see if any of the optional
+    // arguments have been passed
     loop {
         if let Some(arg) = args_iter.next() {
             if arg == "--title" || arg == "-t" {
                 title = Some(args_iter.next().expect("No title specified."));
+            } else if arg == "--help" {
+                print_help();
+                return;
             } else {
-                essential_args.push(arg);
+                match essential_args.len() < 2 {
+                    true => { essential_args.push(arg); }
+                    false => { println!("Additional non-optional argument \"{}\" supplied. Discarding argument. Disregarding argument.", &arg); }
+                }
             }
         } else {
             break;
         }
     }
 
-    let input: String =
-        fs::read_to_string(&essential_args[0]).expect("Error reading file to string");
+    match essential_args.len() {
+        0 => {
+            println!("Must specify both an input and output file.");
+            return;
+        },
+        1 => {
+            println!("Must specify an output file.");
+            return;
+        },
+        _ => {}
+    }
+
+
+    let input = match fs::read_to_string(&essential_args[0]) {
+        Ok(data) =>  data,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => { println!("No such input file \"{}\" found.", &essential_args[0]); return; },
+            _ => { println!("Unrecoverable error when reading input file."); return; }
+        }
+    };
     let mut output = String::new();
     for line in input.lines() {
         let converted = convert_line(line, &mut title);
@@ -35,6 +61,16 @@ fn main() {
         .expect("Error writing to specified output file");
 }
 
+fn print_help() {
+    println!("postit - used to parse a markdown page into an HTML page.
+Usage: postit [OPTIONS] input output
+Options:
+    -t, --title         Set the title of the HTML page. Overrides auto h1 title(s).
+");
+}
+
+/* Fills out a basic HTML template with the content generated during the 
+ * */
 fn fill_html(page_title: &str, heading: &str) -> String {
     format!(
         "<!DOCTYPE html>
@@ -80,6 +116,25 @@ fn resolve_special(line: &str) -> String {
     return converted;
 }
 
+fn resolve_id(line: &str) -> Option<&str> {
+    let mut found_curly_start = false;
+    let mut start = 0;
+    for (i, sym) in line.chars().enumerate() {
+        if sym == '{' {
+            found_curly_start = true;
+            start = i;
+        }
+        if (sym == '}') && (found_curly_start == true) {
+            return Some(&line[start..=i]);
+        }
+    }
+    return None;
+}
+
+fn resolve_class(line: &str) -> Option<&str> {
+    return None;
+} 
+
 fn split_first_space(line: &str) -> (&str, &str) {
     let symbols = line.chars().collect::<Vec<char>>();
     for (i, sym) in symbols.iter().enumerate() {
@@ -91,6 +146,16 @@ fn split_first_space(line: &str) -> (&str, &str) {
         }
     }
     return (&line[0..], "");
+}
+
+fn construct_tag_full(rest: &str, tag_type: String) -> String {
+    return format!(
+        "<{tag} class=\"{class}\" id=\"{id}\">{content}</{tag}>",
+        tag = tag_type,
+        id = "test",
+        content = &resolve_special(rest),
+        class = "s"
+    );
 }
 
 fn convert_line(line: &str, title: &mut Option<String>) -> String {
